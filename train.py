3e6bfd121433a885d21d228b9ba7bcf40bf17cda
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-01-25 23:33:10 +0900

    rm minatar_utils/models.py

--- train.py
+++ train.py
@@ -9,7 +9,6 @@ import torch
 import torch.optim as optim
 
 from minatar import Environment
-from minatar_utils.models import ACNetwork
 from minatar_utils.wrappers import MinAtarEnv
 
 import wandb
@@ -37,6 +36,35 @@ class MinAtarConfig(BaseModel):
     debug: bool = False
     githash: str = git.Repo().head.object.hexsha[:7]
 
+
+class ACNetwork(nn.Module):
+    """Modified from MinAtar example: 
+      - https://github.com/kenjyoung/MinAtar/blob/master/examples/AC_lambda.py
+    """
+    def __init__(self, in_channels, num_actions, env_name):
+        super(ACNetwork, self).__init__()
+        
+        def size_linear_unit(size, kernel_size=3, stride=1):
+            return (size - (kernel_size - 1) - 1) // stride + 1
+
+        self.conv = nn.Conv2d(in_channels, 16, kernel_size=3, stride=1)
+        num_linear_units = size_linear_unit(10) * size_linear_unit(10) * 16
+        self.fc_hidden = nn.Linear(in_features=num_linear_units, out_features=128)
+
+        self.policy = nn.Linear(in_features=128, out_features=num_actions)
+        self.value = nn.Linear(in_features=128, out_features=1)
+        nn.init.constant_(self.value.bias, 0.0)
+        nn.init.constant_(self.value.weight, 0.0)
+
+    # As per implementation instructions, the forward function should be overwritten by all subclasses
+    def forward(self, x):
+        dSiLU = lambda x: torch.sigmoid(x) * (1 + x * (1 - torch.sigmoid(x)))
+        SiLU = lambda x: x * torch.sigmoid(x)
+        
+        x = x.reshape((x.shape[0], -1, 10, 10))  # (n_samples, channels in env, 10, 10)
+        x = SiLU(self.conv(x))
+        x = dSiLU(self.fc_hidden(x.view(x.size(0), -1)))
+        return self.policy(x), self.value(x)
 
 class A2C:
     def __init__(self, config: MinAtarConfig):
