36644f372fe52016b37018d5521bdd7e52e88fae
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-01-25 23:06:07 +0900

    update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
 for ent_coef in 0.0 0.01 0.03 0.1 0.3; do
+for seed in $(seq 1 5); do
 for game in "asterix" "breakout" "freeway" "seaquest" "space_invaders"; do
-    python3 -O train.py game=$game ent_coef=$ent_coef &
+    python3 -O train.py game=$game ent_coef=$ent_coef seed=$seed &
 done
 wait
+done
 done
