416ab22a33332fa0810d55d9bf19dbb1900126b2
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2023-01-26 00:14:33 +0900

    bench minatar_version=v0

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 for ent_coef in 0.0 0.01 0.03 0.1 0.3; do
 for seed in $(seq 1 5); do
 for game in "asterix" "breakout" "freeway" "seaquest" "space_invaders"; do
-    python3 -O train.py game=$game ent_coef=$ent_coef seed=$seed &
+    python3 -O train.py game=$game ent_coef=$ent_coef seed=$seed minatar_version=v0 &
 done
 wait
 done
